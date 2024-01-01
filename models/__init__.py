66c2e2337e27ad250de9535dd44c61108575b13b
Maxrutto <ruttomaxmillan@gmail.com>
2024-01-01 08:13:58 -0800

    Module fixing

--- models/__init__.py
+++ models/__init__.py
@@ -2,6 +2,6 @@
 """ Creates a unique FileStorage instance
 """
 
-from models.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
