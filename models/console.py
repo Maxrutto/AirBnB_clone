e99036c0c9fe6d95d5bf412832c682eb744b0ac2
Maxrutto <ruttomaxmillan@gmail.com>
2024-01-07 15:59:37 +0300

    The console entry

---
+++ models/console.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+"""Defines the HBnB console"""
+import cmd
+from models import storage
+
+class HBnBCommand(cmd.cmd):
+    """Defines the HBnB command interpreter.
+    Attributes:
+        prompt (str): The prompt given to the user/developer
+    """
+
+    __classes = {
+        "BaseModel"
+    }
+
+    def emptyline(self):
+        """Do nothing when receiving an empty line."""
+        pass
+
+    def do_quit(self):
+        """Quit to exit the program."""
+        return True
+
+    def do_EOF(self):
+        """EOF Signal to exit the program"""
+        print("")
+        return True
+
+    def help_help(self):
+        """Prints the description of the help command"""
+        print("Provides description of a given command")
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
