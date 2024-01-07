bc265d07695399de8fdcfb82937b7aa73e0d5d66
Maxrutto <ruttomaxmillan@gmail.com>
2024-01-07 05:32:45 -0800

    Fixing more bugs

--- console.py
+++ console.py
@@ -9,10 +9,6 @@ class HBnBCommand(cmd.Cmd):
         prompt (str): The prompt given to the user/developer
     """
 
-    __classes = {
-        "BaseModel"
-    }
-
     def emptyline(self):
         """Do nothing when receiving an empty line."""
         pass
@@ -25,10 +21,6 @@ class HBnBCommand(cmd.Cmd):
         """EOF Signal to exit the program with a command"""
         print("")
         return True
-
-    def help_help(self):
-        """Prints the description of the help command"""
-        print("Provides description of a given command")
 
 
 if __name__ == '__main__':
